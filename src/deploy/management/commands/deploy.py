c745955ec2e852f112f583691973518f7f47b70f
Esteban <n4b3ts3@gmail.com>
2022-06-24 12:44:25 -0400

    Added support for wsgi, added /media/ to nginx conf file added a more comprehensive readme file

--- src/deploy/management/commands/deploy.py
+++ src/deploy/management/commands/deploy.py
@@ -10,21 +10,24 @@ from django.core import checks
 import sys
 import os
 
+
 # Data of the service file
+wsgi_part = ""
+asgi_part = "-k uvicorn.workers.UvicornWorker --workers 3"
+asgi = "asgi"
+wsgi = "wsgi"
+
 service_file = """
 [Unit]
-Description=Daemon for asgi server
-Requires={0}_django_asgi.socket
+Description=Daemon for {4} server
+Requires={0}_django_{4}.socket
 After=network.target
 
 [Service]
 User=www-data
 Group=www-data
 WorkingDirectory=/var/www/{1}
-ExecStart=/var/www/{1}/venv/bin/gunicorn --access-logfile {2} -k uvicorn.workers.UvicornWorker\
-    --workers 3\
-    --bind unix:/run/{0}_django_asgi.sock\
-    {2}.asgi:application
+ExecStart=/var/www/{1}/venv/bin/gunicorn --access-logfile {3} {5} --bind unix:/run/{0}_django_{4}.sock {2}.{4}:application
 [Install]
 WantedBy=multi-user.target
 """
@@ -34,7 +37,7 @@ socket_file = """
 Description=Gunicorn Socket for {1} Web Server
 
 [Socket]
-ListenStream=/run/{0}_django_asgi.sock
+ListenStream=/run/{0}_django_{2}.sock
 
 [Install]
 WantedBy=sockets.target
@@ -44,17 +47,24 @@ nginx_file = """
 server {
 	listen 80;
 	listen [::]:80;
-	#listen 443 ssl;
-    #listen [::]:443 ssl;
+    #ssl listen 443 ssl;
+    #ssl listen [::]:443 ssl;
+    #ssl ssl_certificate /etc/letsencrypt/live/vpn.expresscuba.com/cert.pem;
+	#ssl ssl_certificate_key /etc/letsencrypt/live/vpn.expresscuba.com/privkey.pem;
 	server_name {2};
 	location = /favicon.ico { access_log off; log_not_found on; }
 	location /static/ {
 		root /var/www/{1};
 	}
+    location /media/ {
+		root /var/www/{1};
+	}
 	location / {
 		include proxy_params;
-		proxy_pass http://unix:/run/{0}_django_asgi.sock;
+		proxy_pass http://unix:/run/{0}_django_{3}.sock;
+        client_max_body_size 10M;
 	}
+
 }
 """
 
@@ -68,15 +78,23 @@ config_file = {
     "integrity": "",
     "date":"", 
 }
+
+
 class Command(BaseCommand):
+    """
+        This commands allows django programmers to quickly deploy a django app for asgi web servers
+    """
     app_name = ""
-    def __init__(self, stdout: Optional[TextIO] = ..., stderr: Optional[TextIO] = ..., no_color: bool = ..., force_color: bool = ...) -> None:
-        self.app_name = settings.DEPLOY_NAME
-        settings.DEPLOY_NAME = md5(str(settings.DEPLOY_NAME).lower().encode()).hexdigest()
-        super().__init__()
     etc_path = "/etc/systemd/"
     nginx_path = "/etc/nginx/"
     help = "Deploy a django project to nginx using asgi"
+
+    def __init__(self, stdout: Optional[TextIO] = ..., stderr: Optional[TextIO] = ..., no_color: bool = ..., force_color: bool = ...) -> None:
+        assert hasattr(settings, "DEPLOY_NAME"), "Configuration exception, settings must have declared DEPLOY_NAME before using this command"
+        self.app_name = settings.DEPLOY_NAME
+        settings.DEPLOY_NAME = settings.DEPLOY_NAME.lower()
+        super().__init__()
+
     def check_deployment(self, args, options):
         self.stdout.write("Doing deployment checks... Please fix any issues before continuing...", style_func=self.style.SQL_TABLE, )
         if args:
@@ -140,19 +158,32 @@ class Command(BaseCommand):
             action="store_true",
             help="Deploy this project with ssl enabled",
         )
+        parser.add_argument(
+            "--dry-run",
+            action="store_true",
+            help="Use symbolics links instead of really copying the file..",
+        )
+        parser.add_argument(
+            "--wsgi",
+            action="store_true",
+            help="Use wsgi instead of asgi",
+        )
 
     def install_services(self, args, options):
-        global service_file, socket_file, nginx_file
-        service_file = service_file.format(settings.DEPLOY_NAME, self.app_name, options["name"] or settings.BASE_DIR.name, 
-                options["log"] or "/tmp/" + settings.DEPLOY_NAME + "_django_asgi")
-        socket_file = socket_file.format(settings.DEPLOY_NAME, settings.BASE_DIR.name)
+        global service_file, socket_file, nginx_file, wsgi, asgi, asgi_part, wsgi_part
+        mode = wsgi if options.get("wsgi", False) else asgi
+        service_file = service_file.format(settings.DEPLOY_NAME, self.app_name, 
+            options.get("name", False) or settings.BASE_DIR.name, 
+            options.get("log", None) or "/tmp/" + settings.DEPLOY_NAME + "_django_" + mode, 
+            mode, wsgi_part if options.get("wsgi", False) else asgi_part)
+        socket_file = socket_file.format(settings.DEPLOY_NAME, settings.BASE_DIR.name, mode)
         nginx_file = nginx_file.replace("{0}", settings.DEPLOY_NAME).replace("{1}", 
-            self.app_name).replace("{2}", options["host"])
-        path = "system/{0}_django_asgi"
+            self.app_name).replace("{2}", options["host"].lower()).replace("{3}", mode)
+        path = "system/{0}_django_"+ mode
         service_path = os.path.join(self.etc_path, path.format(settings.DEPLOY_NAME) + ".service")
         socket_path = os.path.join(self.etc_path, path.format(settings.DEPLOY_NAME) + ".socket")
-        nginx_enabled_path = os.path.join(self.nginx_path, "sites-enabled", settings.DEPLOY_NAME + "_django_asgi")
-        nginx_path = os.path.join(self.nginx_path, "sites-available", settings.DEPLOY_NAME + "_django_asgi")
+        nginx_enabled_path = os.path.join(self.nginx_path, "sites-enabled", settings.DEPLOY_NAME + "_django_" + mode)
+        nginx_path = os.path.join(self.nginx_path, "sites-available", settings.DEPLOY_NAME + "_django_" + mode)
         httpd_path = os.path.join("/var/www/", self.app_name)
         if os.path.exists(service_path) or os.path.exists(socket_path) or os.path.exists(nginx_path):
             if options["fresh"]:
@@ -169,6 +200,8 @@ class Command(BaseCommand):
                     .format(settings.DEPLOY_NAME.lower()))
                 self.stdout.write("path of service is: {}".format(service_path), style_func=self.style.WARNING)
                 sys.exit(2)
+        
+        # Now saving configuration files
         with open(service_path, "x") as service:
             service.write(service_file)
             self.stdout.write("Service was created successfully at path: " + service_path, style_func=self.style.SUCCESS)
@@ -182,22 +215,40 @@ class Command(BaseCommand):
             with open(nginx_enabled_path, "x") as nginx_enabled:
                 nginx_enabled.write(nginx_file)
             self.stdout.write("Nginx configuration file was created successfully at path: " + nginx_path, style_func=self.style.SUCCESS)
+        
         if not os.system("systemctl daemon-reload") == 0:
             self.stderr.write("Something ocurred while loading the configurations files... \
                 please report this at https://github.com/n4b3ts3/django-deploy")
             sys.exit(6)
         self.stdout.write("All configurations files were successfully created and loaded... now passing to action!!!", style_func=self.style.SUCCESS)
-        
-        if os.path.exists(httpd_path) and round(os.path.getsize(httpd_path)/10) == round(os.path.getsize(settings.BASE_DIR)/10):
-            self.stdout.write("Project already exists skipping moving to /var/www/...", self.style.WARNING)
-        elif os.system("cp -r {0} {1}".format(settings.BASE_DIR, httpd_path)) == 0:
+        exists = os.path.exists(httpd_path) and round(os.path.getsize(httpd_path)/10) == round(os.path.getsize(settings.BASE_DIR)/10)
+        if options.get("dry_run", False):
+            if not exists:
+                if os.system("ln -s {0} {1}".format(settings.BASE_DIR, httpd_path)) == 0:
+                    self.stdout.write("Link created successfully...", self.style.SUCCESS)
+                else:
+                    raise CommandError("Cannot successfully create link at {}".format(httpd_path))
+            else:
+                self.stdout.write("Cannot use dry-run because path does exists, skipping instead...", self.style.WARNING)
+        elif (options.get("fresh", False) or not exists ):
+            if options.get("fresh", False) and exists:
+                if os.path.islink(httpd_path) or os.path.isfile(httpd_path):
+                    os.remove(httpd_path)
+                elif os.path.isdir(httpd_path):
+                    raise CommandError("{0} is not empty for security reasons and a little bit of paranoid please delete it manually".format(httpd_path))
+                else:
+                    raise CommandError("{0} is not either a file a link or a folder... please check this manually".format(httpd_path))
+            os.system("cp -r {0} {1}".format(settings.BASE_DIR, httpd_path)) == 0
             self.stdout.write("Project copied to {}".format(httpd_path), self.style.SUCCESS)
+        elif exists:
+            self.stdout.write("Project already exists skipping moving to /var/www/...", self.style.WARNING)
         else:
             self.stderr.write("Cannot successfully copy content of project into {}".format(httpd_path))
-        self.stdout.write("Starting {0} with service name {1}_django_asgi".format(self.app_name, settings.DEPLOY_NAME))
-        if os.system("systemctl start {}_django_asgi".format(settings.DEPLOY_NAME)) == 0:
-            self.stdout.write("Django ASGI server service started, use 'systemctl stop {0}_django_asgi' for stop it"
-                .format(settings.DEPLOY_NAME), 
+        
+        self.stdout.write("Starting {0} with service name {1}_django_{2}".format(self.app_name, settings.DEPLOY_NAME, mode))
+        if os.system("systemctl start {0}_django_{1}".format(settings.DEPLOY_NAME, mode)) == 0:
+            self.stdout.write("Django {1} server service started, use 'systemctl stop {0}_django_asgi' for stop it"
+                .format(settings.DEPLOY_NAME, mode.upper()), 
                 style_func=self.style.SUCCESS)
         else:
             self.stderr.write("Cannot successfully initiate the django asgi service try again later...")
